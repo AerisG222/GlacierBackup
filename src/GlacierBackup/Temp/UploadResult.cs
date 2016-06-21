36b2d1afcd0efafa058da276206af98c15198fc1
Mike Morano <mmorano@mikeandwan.us>
2016-06-20 21:12:41 -0400

    amazon.glacier.transfer (high level api) is not currently supported.  add this temporary implementation based on their wip

---
+++ src/GlacierBackup/Temp/UploadResult.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace GlacierBackup.Temp
+{
+    class UploadResult
+    {
+        public string ArchiveId { get; set; }
+        public string Checksum { get; set; }
+    }
+}
