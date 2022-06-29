48dec02f9eb1c4edaf42fb0ff42597190d0b77d1
Mike Morano <mmorano@mikeandwan.us>
2022-06-29 07:41:49 -0400

    set this to try and improve upload perf by allowing more concurrent connections

--- src/GlacierBackup/Program.cs
+++ src/GlacierBackup/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Amazon;
@@ -141,6 +142,8 @@ public class Program
             OutputType = theOutputType,
             Output = output
         });
+
+        ServicePointManager.DefaultConnectionLimit = 50;
 
         program.Execute();
     }
